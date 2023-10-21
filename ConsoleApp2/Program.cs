a3cf2ee5bc14589fd268746658489c8bcad4f34d
FourTeen <pavel20001502@gmail.com>
2023-10-21 19:30:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Net.NetworkInformation;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string targetIP = "46.174.52.237";
+        int pingIntervalMinutes = 1; 
+        using (Ping ping = new Ping())
+        {
+            while (true)
+            {
+                PingReply reply = ping.Send(targetIP);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine($"Ping успешен. Время ответа: {reply.RoundtripTime} мс");
+                }
+                else
+                {
+                    Console.WriteLine($"Ping не удался. Состояние: {reply.Status}");
+                }
+                Thread.Sleep(pingIntervalMinutes * 60 * 1000); 
+            }
+        }
+    }
+}
